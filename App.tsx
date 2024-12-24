af4da817110d8c3546a3708dedd15966ef3e02a5
chirag-unision <sharmacj123@gmail.com>
2024-12-24 20:25:34 +0530

    final changes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-
-import {
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native';
-
+import React, { useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import {PermissionsAndroid} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native';
+
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 function App(): React.JSX.Element {
-  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+  useEffect(() => {
+    init();
+
+    return () => {
+
+    }
+  }, [])
+
+  
+  const init= async () => {
+    const token = await messaging().getToken();
+    console.log('FCM Token:', token);
+  };
+
+  messaging().onMessage(async remoteMessage => {
+    console.log('Notification received in foreground:', remoteMessage);
+    pushNotice(remoteMessage.notification?.title, remoteMessage.notification?.body);
+  });
+
+  const options = {
+    taskName: 'Example',
+    taskTitle: 'ExampleTask title',
+    taskDesc: 'ExampleTask description',
+    taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+    },
+    color: '#ff00ff',
+    linkingURI: '/',
+    parameters: {
+        delay: 10000,
+    },
+  };
+
+  const pushNotice = async (title:string, body:string) => {
+    await notifee.requestPermission()
+
+    const channelId = await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+    });
+
+    notifee.displayNotification({
+        title: title,
+        body: body,
+        android: {
+          channelId,
+          // smallIcon: 'name-of-a-small-icon',
+          pressAction: {
+            id: 'default',
+          },
+        },
+    })
+  }
 
   return (
-    <SafeAreaView>
-
+    <SafeAreaView style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.text}>Hey there!</Text>
+          <Text style={styles.text}>This is FCM App</Text>
+        </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    backgroundColor: '#fff',
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  section: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1
+  },
+  text: {
+    fontSize: 20,
+    padding: 10
+  }
 });
 
 export default App;
